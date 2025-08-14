7570b9d9e3dd4ded661bb6a603ec3d944f26d8f3
yansuming <yansuming@qq.com>
2025-08-14 11:54:18 +0800

    正确的返回值，通过过滤器进行检查并统一包装，便于前端接收并处理。

--- CoffeeScaffolding/Controllers/Filters/ActionFilter.cs
+++ CoffeeScaffolding/Controllers/Filters/ActionFilter.cs
@@ -29,20 +29,40 @@ namespace CoffeeScaffolding.Controllers.Filters
                 executedContext.ExceptionHandled = true; // 标记异常已处理
                 sw.Stop();
                 _logger.LogError(executedContext.Exception, "Action error {Action} in {ElapsedMs} ms", context.ActionDescriptor.DisplayName, sw.ElapsedMilliseconds);
-                ComResp resp = new ComResp(FlagEnum.Error, new { ErrorMsg = executedContext.Exception.Message });
+                ComResp resp = new ComResp(FlagEnum.Fail, new { ErrorMsg = executedContext.Exception.Message });
                 executedContext.Result = new JsonResult(resp);
-                executedContext.HttpContext.Response.StatusCode = 500; 
+                executedContext.HttpContext.Response.StatusCode = 500;
             }
             else
             {
+                // 成功时统一包装返回结果（避免重复包装）
+                var result = executedContext.Result;
+                switch (result)
+                {
+                    case ObjectResult obj when obj.Value is ComResp:
+                        break; // 已经是 ComResp
+                    case JsonResult json when json.Value is ComResp:
+                        break; // 已经是 ComResp
+                    case ObjectResult obj:
+                        executedContext.Result = new JsonResult(new ComResp(FlagEnum.Error, obj.Value ?? new object()));
+                        break;
+                    case JsonResult json:
+                        executedContext.Result = new JsonResult(new ComResp(FlagEnum.Error, json.Value ?? new object()));
+                        break;
+                    case EmptyResult:
+                        executedContext.Result = new JsonResult(new ComResp(FlagEnum.Error, new object()));
+                        break;
+                    case StatusCodeResult statusRes:
+                        executedContext.Result = new JsonResult(new ComResp(FlagEnum.Error, new { statusRes.StatusCode }));
+                        break;
+                    default:
+                    // 其他诸如 FileResult/ChallengeResult/PhysicalFileResult 等保持不变，用于处理文件返回值
+                        break;
+                }
+
                 sw.Stop();
                 _logger.LogInformation("Action finished {Action} in {ElapsedMs} ms with {Status}", action, sw.ElapsedMilliseconds, executedContext.HttpContext.Response?.StatusCode);
             }
-        }
-
-        public void OnActionExecuted(ActionExecutedContext context)
-        { 
-            
         }
     }
 }
